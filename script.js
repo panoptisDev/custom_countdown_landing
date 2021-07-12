be08505076e19187c9e0cf8d6f382df8f6b7b04f
doniwirawan <doniwirawan166@gmail.com>
2021-07-12 15:20:33 +0800

    adding a whole file

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const dateEl = document.getElementById('date-picker')
 const countdownEl = document.getElementById('countdown')
 const countdownElTitle = document.getElementById('countdown-title')
 const countdownBtn = document.getElementById('countdown-button')
-const timeElements = document.querySelectorAll('span')
+const timeElements = document.querySelectorAll('.time')
 
 
 const completeEl = document.getElementById('complete')
